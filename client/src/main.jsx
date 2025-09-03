2ca3b6c3782f362d5410f4cbd7e8f0cfbc496727
ParthKhiriya <b23ee1051@iitj.ac.in>
2025-09-03 18:29:03 +0530

    Completed getting online users part and implemented the seen, unseen messages functionality

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,13 +3,16 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
-import { AuthProvider } from '../context/AuthContext.jsx';
+import { AuthProvider } from '../context/AuthContext.jsx'
+import { ChatProvider } from '../context/ChatContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <App />
+        <ChatProvider>
+          <App />
+        </ChatProvider>
       </AuthProvider>
     </BrowserRouter>
   </StrictMode>,
--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,22 +1,27 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import assets from '../assets/assets'
 import { useState } from 'react'
+import { AuthContext } from '../../context/AuthContext';
 
 const LoginPage = () => {
 
-  const [currentState, setCurrentState] = useState("Sign Up");
+  const [currentState, setCurrentState] = useState("Sign up");
   const [fullName, setFullName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [bio, setBio] = useState("");
   const [isDataSubmitted, setIsDataSubmitted] = useState(false);
 
+  const {login} = useContext(AuthContext);
+
   const onSubmitHandler = (event) => {
     event.preventDefault();
-    if(currentState === "Sign Up" && !isDataSubmitted) {
+    if(currentState === "Sign up" && !isDataSubmitted) {
       setIsDataSubmitted(true)
       return;
     }
+
+    login(currentState === "Sign up" ? 'signup' : 'login', {fullName, email, password, bio});
   }
 
   return (
